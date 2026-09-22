d3d5399dae15be4f8a8a0c6efcfbabe2ddd818f8
real69clown <fiogxs42@gmail.com>
2026-09-22 14:59:55 +0300

    Lab05

--- Program.cs
+++ Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вариант 5
+// int weather = 16;
+
+// switch (weather) {
+//     case < 0:
+//         Console.WriteLine("Мороз");
+//         break;
+//     case >= 0 and <= 9:
+//         Console.WriteLine("Прохладно");
+//         break;
+//     case >= 10 and <= 19:
+//         Console.WriteLine("Комфортно");
+//         break;
+//     case >= 20 and <= 29:
+//         Console.WriteLine("Тепло");
+//         break;
+//     case >= 30:
+//         Console.WriteLine("Жарко");
+//         break;
+// }
+
+// Вариант 2
+// int score = 80;
+
+// switch (score) {
+//      case >= 0 and <=39:
+//          Console.WriteLine("Неудовлетворительно");
+//          break;
+//      case >= 40 and <= 59:
+//          Console.WriteLine("Удовлетворительно");
+//          break;
+//      case >= 60 and <= 79:
+//          Console.WriteLine("Хорошо");
+//          break;
+//      case >= 80 and <= 100:
+//          Console.WriteLine("Отлично");
+//          break;
+//  }
+
+// Задача Б
+int age = 16;
+
+ switch (age) {
+     case < 0:
+         Console.WriteLine("Ошибка");
+         break;
+     case >= 0 and <= 6:
+         Console.WriteLine("Ребёнок");
+         break;
+     case >= 7 and <= 17:
+         Console.WriteLine("Подросток");
+         break;
+     case >= 18 and <= 64:
+         Console.WriteLine("Взрослый");
+         break;
+     case >= 65:
+         Console.WriteLine("Пенсионер");
+         break;
+ }
